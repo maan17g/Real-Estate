1fabff985230ac894193c5ea07067b275a4c78d4
maan17g <zoxo.avi@gmail.com>
2026-01-26 15:44:52 +0500

    Initital commit

---
+++ public/js/script.js
@@ -0,0 +1,40 @@
+// 1. Theme Toggle Logic
+const themeBtn = document.getElementById("themeToggle");
+const themeIcon = themeBtn.querySelector("i");
+const body = document.body;
+
+themeBtn.addEventListener("click", () => {
+  const currentTheme = body.getAttribute("data-theme");
+
+  if (currentTheme === "dark") {
+    body.setAttribute("data-theme", "light");
+    themeIcon.classList.replace("bi-sun-fill", "bi-moon-fill");
+  } else {
+    body.setAttribute("data-theme", "dark");
+    themeIcon.classList.replace("bi-moon-fill", "bi-sun-fill");
+  }
+});
+
+// 2. Navbar Scroll Effect
+const navbar = document.getElementById("navbar");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("nav-scroll");
+  } else {
+    navbar.classList.remove("nav-scroll");
+  }
+});
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  const start = +counter.dataset.bsStart;
+  const end = +counter.dataset.bsEnd;
+  const sign = counter.dataset.bsSign;
+  let i = start;
+
+  const interval = setInterval(() => {
+    counter.innerHTML = i + sign;
+    i++;
+    if (i > end) clearInterval(interval);
+  }, 10);
+});
