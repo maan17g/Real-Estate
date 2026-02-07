bc5f3451ad63040bbc496600a4f0c26012e0e6d5
Aman Ahmad <zoxo.avi@gmail.com>
2026-02-07 13:07:13 +0500

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,50 +1,13 @@
-// 1. Theme Toggle Logic
-const themeBtn = document.getElementById("themeToggle");
-const themeIcon = themeBtn.querySelector("i");
-const body = document.body;
-
-themeBtn.addEventListener("click", () => {
-  const currentTheme = body.getAttribute("data-theme");
-
-  if (currentTheme === "dark") {
-    body.setAttribute("data-theme", "light");
-    themeIcon.classList.replace("bi-sun-fill", "bi-moon-fill");
-  } else {
-    body.setAttribute("data-theme", "dark");
-    themeIcon.classList.replace("bi-moon-fill", "bi-sun-fill");
-  }
-});
-
-// 2. Navbar Scroll Effect
-const navbar = document.getElementById("navbar");
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    navbar.classList.add("nav-scroll");
-  } else {
-    navbar.classList.remove("nav-scroll");
-  }
-});
 const counters = document.querySelectorAll(".counter");
-
-counters.forEach((counter) => {
+let totalsec=2;
+counters.forEach(counter => {
   const start = +counter.dataset.bsStart;
   const end = +counter.dataset.bsEnd;
-  const sign = counter.dataset.bsSign;
+  const sign= counter.dataset.bsSign;
   let i = start;
-
-  const interval = setInterval(() => {
-    counter.innerHTML = i + sign;
+ const interval=setInterval(() => {
+    counter.innerHTML=i+sign;
     i++;
-    if (i > end) clearInterval(interval);
-  }, 10);
+    if(i>end) clearInterval(interval);
+  }, 1000/(end/totalsec));
 });
-
-  window.addEventListener("load", () => {
-    const preloader = document.getElementById("preloader");
-    preloader.classList.add("preloader-hide");
-
-    setTimeout(() => {
-      preloader.style.display = "none";
-    }, 800);
-  });
-
